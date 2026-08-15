25d8384022948fe3b51d869b439f7f6631eb7dc2
166405241035-st-cyber <166405241035-st@rmutsb.ac.th>
2026-08-15 15:17:04 +0700

    Add forging block interaction

---
+++ src/main/java/com/example/examplemod/ForgingBlock.java
@@ -0,0 +1,25 @@
+package com.example.examplemod;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+
+public class ForgingBlock extends Block {
+    public ForgingBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player,
+            BlockHitResult hitResult) {
+        if (level.isClientSide()) {
+            ForgingClientHooks.openForgingScreen();
+        }
+        return InteractionResult.sidedSuccess(level.isClientSide());
+    }
+}
