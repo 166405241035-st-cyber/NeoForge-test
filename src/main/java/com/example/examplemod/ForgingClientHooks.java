aa383fef78b0ec3da9e7afe51747de7d1c70a40c
166405241035-st-cyber <166405241035-st@rmutsb.ac.th>
2026-08-15 15:17:10 +0700

    Add client hook for forging screen

---
+++ src/main/java/com/example/examplemod/ForgingClientHooks.java
@@ -0,0 +1,12 @@
+package com.example.examplemod;
+
+import net.minecraft.client.Minecraft;
+
+public final class ForgingClientHooks {
+    private ForgingClientHooks() {
+    }
+
+    public static void openForgingScreen() {
+        Minecraft.getInstance().setScreen(new ForgingScreen());
+    }
+}
