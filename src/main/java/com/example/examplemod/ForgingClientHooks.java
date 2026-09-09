ac9939ec69e38cc46461dc603f527fe510a7c8e8
166405241035-st-cyber <166405241035-st@rmutsb.ac.th>
2026-09-09 21:23:24 +0700

    Remove obsolete direct forge screen constructor

--- src/main/java/com/example/examplemod/ForgingClientHooks.java
+++ src/main/java/com/example/examplemod/ForgingClientHooks.java
@@ -6,10 +6,6 @@ public final class ForgingClientHooks {
     private ForgingClientHooks() {
     }
 
-    public static void openForgingScreen() {
-        Minecraft.getInstance().setScreen(new ForgingScreen());
-    }
-
     public static void openAnvilScreen() {
         Minecraft.getInstance().setScreen(new ForgingAnvilScreen());
     }
