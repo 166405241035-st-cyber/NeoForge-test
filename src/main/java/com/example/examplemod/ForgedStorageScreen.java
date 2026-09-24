5bda2891eec0c43de8c347b7b790a6b482b9ccea
166405241035-st-cyber <166405241035-st@rmutsb.ac.th>
2026-09-24 11:58:39 +0700

    Fix forged storage screen slot access

--- src/main/java/com/example/examplemod/ForgedStorageScreen.java
+++ src/main/java/com/example/examplemod/ForgedStorageScreen.java
@@ -8,14 +8,14 @@ import net.minecraft.world.entity.player.Inventory;
 public class ForgedStorageScreen extends AbstractContainerScreen<ForgedStorageMenu> {
     public ForgedStorageScreen(ForgedStorageMenu menu, Inventory inv, Component title) {
         super(menu, inv, title);
-        int rows=(menu.slots.size()-36+8)/9;
+        int rows=(menu.getStorageSlotCount()+8)/9;
         this.imageHeight=114+rows*18;
         this.inventoryLabelY=this.imageHeight-94;
     }
     @Override protected void renderBg(GuiGraphics g,float partial,int mx,int my){
         int x=leftPos,y=topPos;
         g.fill(x,y,x+imageWidth,y+imageHeight,0xFF303030);
-        int storageSlots=menu.slots.size()-36;
+        int storageSlots=menu.getStorageSlotCount();
         for(int i=0;i<storageSlots;i++){
             int sx=x+7+(i%9)*18, sy=y+17+(i/9)*18;
             g.fill(sx,sy,sx+18,sy+18,0xFF101010);
