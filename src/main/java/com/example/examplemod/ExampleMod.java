6726f164dc75a84d070cd4daeae6629fc0717ea0
166405241035-st-cyber <166405241035-st@rmutsb.ac.th>
2026-09-25 20:08:51 +0700

    Register invisible support block

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -52,6 +52,7 @@ public class ExampleMod {
     public static final DeferredBlock<Block> FORGING_ANVIL = BLOCKS.register("forging_anvil", registryName -> new ForgingAnvilBlock(BlockBehaviour.Properties.of().mapColor(MapColor.METAL).strength(5.0F)));
     public static final DeferredItem<BlockItem> FORGING_ANVIL_ITEM = ITEMS.registerSimpleBlockItem("forging_anvil", FORGING_ANVIL);
     public static final DeferredBlock<Block> EQUIPMENT_TEST_BLOCK = BLOCKS.register("equipment_test_block", registryName -> new EquipmentTestBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_PURPLE).strength(3.5F)));
+    public static final DeferredBlock<Block> INVISIBLE_SUPPORT_BLOCK = BLOCKS.register("invisible_support", registryName -> new InvisibleSupportBlock(BlockBehaviour.Properties.of().strength(0.2F).noCollission().noOcclusion()));
     public static final DeferredItem<BlockItem> EQUIPMENT_TEST_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("equipment_test_block", EQUIPMENT_TEST_BLOCK);
     public static final DeferredHolder<EntityType<?>, EntityType<ForgedBoomerangEntity>> FORGED_BOOMERANG = ENTITY_TYPES.register("forged_boomerang",
             () -> EntityType.Builder.<ForgedBoomerangEntity>of(ForgedBoomerangEntity::new, MobCategory.MISC)
