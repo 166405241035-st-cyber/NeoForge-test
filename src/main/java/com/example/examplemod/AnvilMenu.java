d0943520fabf29aaf6696c231b6bb112a8f6698c
166405241035-st-cyber <166405241035-st@rmutsb.ac.th>
2026-09-25 10:04:25 +0700

    Update imports for AnvilMenu.java

--- src/main/java/com/example/examplemod/AnvilMenu.java
+++ src/main/java/com/example/examplemod/AnvilMenu.java
@@ -1,5 +1,11 @@
 package com.example.examplemod;
 
+import com.example.examplemod.block.*;
+import com.example.examplemod.entity.*;
+import com.example.examplemod.item.*;
+import com.example.examplemod.skill.*;
+import com.example.examplemod.skill.client.*;
+
 import net.minecraft.world.Container;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
