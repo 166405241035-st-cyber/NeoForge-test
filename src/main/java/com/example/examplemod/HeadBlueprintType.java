e82f51313addaf87b7aa14801236428e14929dd9
166405241035-st-cyber <166405241035-st@rmutsb.ac.th>
2026-09-25 10:06:48 +0700

    Update imports for HeadBlueprintType.java

--- src/main/java/com/example/examplemod/HeadBlueprintType.java
+++ src/main/java/com/example/examplemod/HeadBlueprintType.java
@@ -1,5 +1,11 @@
 package com.example.examplemod;
 
+import com.example.examplemod.block.*;
+import com.example.examplemod.entity.*;
+import com.example.examplemod.item.*;
+import com.example.examplemod.skill.*;
+import com.example.examplemod.skill.client.*;
+
 import java.util.EnumSet;
 import java.util.Set;
 
