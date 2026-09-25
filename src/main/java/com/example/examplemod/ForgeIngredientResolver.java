b7fad593d8131842c2739d4a9fd33f69631907cc
166405241035-st-cyber <166405241035-st@rmutsb.ac.th>
2026-09-25 10:05:44 +0700

    Update imports for ForgeIngredientResolver.java

--- src/main/java/com/example/examplemod/ForgeIngredientResolver.java
+++ src/main/java/com/example/examplemod/ForgeIngredientResolver.java
@@ -1,5 +1,11 @@
 package com.example.examplemod;
 
+import com.example.examplemod.block.*;
+import com.example.examplemod.entity.*;
+import com.example.examplemod.item.*;
+import com.example.examplemod.skill.*;
+import com.example.examplemod.skill.client.*;
+
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
