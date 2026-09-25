c289853b67bc588c64d7fc549e42211e1c00f2be
166405241035-st-cyber <166405241035-st@rmutsb.ac.th>
2026-09-25 10:05:01 +0700

    Update imports for ExampleModClient.java

--- src/main/java/com/example/examplemod/ExampleModClient.java
+++ src/main/java/com/example/examplemod/ExampleModClient.java
@@ -1,5 +1,11 @@
 package com.example.examplemod;
 
+import com.example.examplemod.block.*;
+import com.example.examplemod.entity.*;
+import com.example.examplemod.item.*;
+import com.example.examplemod.skill.*;
+import com.example.examplemod.skill.client.*;
+
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
