f8d3fbb5568f9aa0b34b0e2d7464a48b0d318797
166405241035-st-cyber <166405241035-st@rmutsb.ac.th>
2026-09-25 10:02:54 +0700

    Set package for BleedingMobEffect.java

--- src/main/java/com/example/examplemod/skill/BleedingMobEffect.java
+++ src/main/java/com/example/examplemod/skill/BleedingMobEffect.java
@@ -1,4 +1,11 @@
-package com.example.examplemod;
+package com.example.examplemod.skill;
+
+import com.example.examplemod.*;
+import com.example.examplemod.block.*;
+import com.example.examplemod.entity.*;
+import com.example.examplemod.item.*;
+import com.example.examplemod.skill.*;
+import com.example.examplemod.skill.client.*;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
